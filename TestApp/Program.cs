0bb16c6ad3b4e244f5418f00149a021561b2a279
Choice <winmain@vip.qq.com>
2016-02-01 15:33:46 +0800

    工程调整，添加测试工程

---
+++ TestApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using IrrlichtLime;
+using IrrlichtLime.Core;
+using IrrlichtLime.Video;
+using IrrlichtLime.Scene;
+using IrrlichtLime.GUI;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IrrlichtDevice device = IrrlichtDevice.CreateDevice(
+                DriverType.Software, new Dimension2Di(640, 480), 16, false, false, false);
+
+            device.SetWindowCaption("Hello World! - Irrlicht Engine Demo");
+
+            VideoDriver driver = device.VideoDriver;
+            SceneManager smgr = device.SceneManager;
+            GUIEnvironment gui = device.GUIEnvironment;
+
+            gui.AddStaticText("Hello World! This is the Irrlicht Software renderer!",
+                new Recti(10, 10, 260, 22), true);
+
+            // 			AnimatedMesh mesh = smgr.GetMesh("../../media/sydney.md2");
+            // 			AnimatedMeshSceneNode node = smgr.AddAnimatedMeshSceneNode(mesh);
+            // 
+            // 			if (node != null)
+            // 			{
+            // 				node.SetMaterialFlag(MaterialFlag.Lighting, false);
+            // 				node.SetMD2Animation(AnimationTypeMD2.Stand);
+            // 				node.SetMaterialTexture(0, driver.GetTexture("../../media/sydney.bmp"));
+            // 			}
+
+            smgr.AddCameraSceneNode(null, new Vector3Df(0, 30, -40), new Vector3Df(0, 5, 0));
+
+            while (device.Run())
+            {
+                driver.BeginScene(true, true, new Color(100, 101, 140));
+
+                smgr.DrawAll();
+                gui.DrawAll();
+
+                driver.EndScene();
+            }
+
+            device.Drop();
+        }
+    }
+}
