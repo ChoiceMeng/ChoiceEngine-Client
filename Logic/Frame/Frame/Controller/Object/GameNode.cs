97fd09cc3ff81be5e8498b276a5b91feca5a2cfb
Choice <winmain@vip.qq.com>
2016-05-03 18:51:41 +0800

    逻辑框架工程添加。

---
+++ Logic/Frame/Frame/Controller/Object/GameNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frame
+{
+    abstract class GameNode
+    {
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        ///   start
+        /// </summary>
+        public abstract bool Start();
+
+        /// <summary>
+        ///   init
+        /// </summary>
+        public abstract bool Init();
+
+        /// <summary>
+        ///   pause
+        /// </summary>
+        public abstract bool Pause();
+
+        /// <summary>
+        ///   stop
+        /// </summary>
+        public abstract bool Stop();
+
+        /// <summary>
+        ///   destroy
+        /// </summary>
+        public abstract bool Destroy();
+    }
+}
